8f58832fc32d93e9feaff439a7daa66de743fe50
ThatPham2000 <thatpham2000@gmail.com>
2020-06-25 22:38:27 +0700

    Tao POJO SinhVien xong

---
+++ QLMH/src/main/java/com/hibernate/entities/SinhVien.java
@@ -0,0 +1,47 @@
+package com.hibernate.entities;
+
+import java.io.Serializable;
+
+public class SinhVien implements Serializable
+{
+	private static final long serialVersionUID = 1L;
+	
+	private String MSSV;
+	private String HoTen;
+	private String GioiTinh;
+	private String CMND;
+	private String MaLop;
+	
+	public String getMSSV() {
+		return MSSV;
+	}
+	public void setMSSV(String mSSV) {
+		MSSV = mSSV;
+	}
+	public String getHoTen() {
+		return HoTen;
+	}
+	public void setHoTen(String hoTen) {
+		HoTen = hoTen;
+	}
+	public String getGioiTinh() {
+		return GioiTinh;
+	}
+	public void setGioiTinh(String gioiTinh) {
+		GioiTinh = gioiTinh;
+	}
+	public String getCMND() {
+		return CMND;
+	}
+	public void setCMND(String cMND) {
+		CMND = cMND;
+	}
+	public String getMaLop() {
+		return MaLop;
+	}
+	public void setMaLop(String maLop) {
+		MaLop = maLop;
+	}
+	
+	
+}
